4b304b2d9a73611608b6db62af6111cd627804ae
bricktrooper <kylepinto@yahoo.ca>
2019-08-23 01:46:25 -0400

    uart: Add error checking to UART functions

--- uart/uart.h
+++ uart/uart.h
@@ -2,7 +2,7 @@
 #define UART_H
 
 void uart_init(void);
-void uart_send(const char * data);
-void uart_receive(char * buffer, unsigned int length);
+int uart_send(const char * data);
+int uart_receive(char * buffer, int length);
 
 #endif /* UART_H */

b42d05815677e44cf10753e63f3c9023131a897d
Kyle Pinto <kylepinto@yahoo.ca>
2020-02-04 23:05:32 -0500

    uart: rename TX function to 'uart_transmit'
    Function was originally named 'uart_send'.

--- uart/uart.h
+++ uart/uart.h
@@ -2,7 +2,7 @@
 #define UART_H
 
 void uart_init(void);
-int uart_send(const char * data);
+int uart_transmit(const char * data);
 int uart_receive(char * buffer, int length);
 
 #endif /* UART_H */

ce566536f5b3da1533b639fd748492535775bea5
Kyle Pinto <kylepinto@yahoo.ca>
2020-02-12 22:12:59 -0500

    uart: Add buffer size to TX function
    Specify the size of the data buffer
    that is to be transmitted on the TX
    line.  This is safer and more secure
    then transmitting until a newline or
    null terminator, which will be handled
    in the serial module's print functions.

--- uart/uart.h
+++ uart/uart.h
@@ -2,7 +2,7 @@
 #define UART_H
 
 void uart_init(void);
-int uart_transmit(const char * data);
+int uart_transmit(const char * data, int length);
 int uart_receive(char * buffer, int length);
 
 #endif /* UART_H */

947773551187f161ef33b69d75845d5f644206e8
Kyle Pinto <kylepinto@yahoo.ca>
2020-02-12 23:55:20 -0500

    serial: Add test code for Serial O/I
    Currently only tests printing over UART.

---
+++ serial/test.c
@@ -0,0 +1,24 @@
+/* Test code for PIC16F690 serial I/O */
+
+#include <xc.h>
+#include <pic16f690.h>
+
+#include "../init/init.h"
+#include "../uart/uart.h"
+#include "serial.h"
+
+void main(void)
+{
+	init_hardware();
+	uart_init();
+
+	serial_println("");
+	serial_print("serial_print(): Hello\r\n");
+	serial_println("serial_println(): Hello");
+	serial_printf("serial_printf(): number = %d\r\n", 7);
+	serial_print("serial_printc(): ");
+	serial_printc('K');
+	serial_println("");
+
+	while (1);
+}

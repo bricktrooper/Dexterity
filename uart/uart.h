4c5e8f01ae5329d4d7c79f08beba7829a828140d
Kyle Pinto <kylepinto@yahoo.ca>
2020-02-19 21:14:19 -0500

    uart: Remove const for readability

--- uart/uart.h
+++ uart/uart.h
@@ -2,9 +2,9 @@
 #define UART_H
 
 void uart_init(void);
-void uart_transmit_byte(const char byte);
+void uart_transmit_byte(char byte);
 char uart_receive_byte(void);
-int uart_transmit_data(const char * data, int length);
+int uart_transmit_data(char * data, int length);
 int uart_receive_data(char * buffer, int length);
 
 #endif /* UART_H */

331a0aa61e28dd742ad9a4cfac8089ab8231582e
Kyle Pinto <kylepinto@yahoo.ca>
2020-02-19 22:03:18 -0500

    serial: Implement serial read function
    Add function to read input from UART
    into a buffer.

--- serial/serial.c
+++ serial/serial.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <xc.h>
 #include <pic16f690.h>
 
@@ -11,19 +12,45 @@
 #define WARNING    1
 #define SUCCESS    0
 
-#define NEWLINE   "\r\n"
-
-int serial_print(const char * string)
+int serial_print(char * string)
 {
 	return serial_printf(string);
 }
 
-int serial_println(const char * string)
+int serial_println(char * string)
 {
 	return serial_printf(string) + serial_printf(NEWLINE);
 }
 
-void putch(const char byte)
+int serial_read(char * input, int length)
 {
-    uart_transmit(&byte, 1);
+	if (input == null || length < 0)
+	{
+		return ERROR;
+	}
+
+	char next_byte = 0;
+	int byte_count = 0;
+	memset(input, 0, length);
+
+	for (int i = 0; i < length - 1; i++)
+	{
+		next_byte = uart_receive_byte();
+
+		if (next_byte == '\r')   // carriage return [Enter] indicates end of transmission
+		{
+
+			break;
+		}
+
+		input[i] = next_byte;    // save new byte in input buffer
+		byte_count++;
+	}
+
+	return byte_count;
+}
+
+void putch(char byte)
+{
+    uart_transmit_byte(byte);
 }
--- serial/serial.h
+++ serial/serial.h
@@ -3,11 +3,15 @@
 
 #include <stdio.h>
 
+#define NEWLINE   "\r\n"
+
 #define serial_printc putch
 #define serial_printf printf
 
-int serial_print(const char * string);
-int serial_println(const char * string);
-void putch(const char byte);
+int serial_print(char * string);
+int serial_println(char * string);
+int serial_scan(char * input, int length);
+int serial_read(char * input, int length);
+void putch(char byte);
 
 #endif /* SERIAL_H */

391d3f8a6a3bb31bb45ce845b562049939673037
Kyle Pinto <kylepinto@yahoo.ca>
2020-02-23 03:09:35 -0500

    adc: Test reading of multiple analogue channels
    Add test code to quickly read from multiple
    analogue channels in succession.

--- adc/adc-test.c
+++ adc/adc-test.c
@@ -16,15 +16,20 @@ void main(void)
 
 	serial_println(NEWLINE NEWLINE "======= ADC Test =======" NEWLINE);
 
+	TRISB4 = 1;   // set RB4 as an input
+	ANS10 = 1;    // set RB4 as analogue
+
 	TRISC7 = 1;   // set RC7 as an input
 	ANS9 = 1;     // set RC7 as analogue
 
-	int reading = 0;
+	int reading_AN9 = 0;
+	int reading_AN10 = 0;
 
 	while (1)
 	{
-		reading = adc_read(9);
-		serial_printf("AN9 Reading: %u" NEWLINE, reading);
+		reading_AN9 = adc_read(9);
+		reading_AN10 = adc_read(10);
+		serial_printf("AN9: %u, AN10: %u" NEWLINE, reading_AN9, reading_AN10);
 		_delay(100000);
 	}
 }

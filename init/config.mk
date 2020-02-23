bb7e9ed9d0655731cdb3a6f416f3a9a774218ddb
Kyle Pinto <kylepinto@yahoo.ca>
2020-02-23 03:01:02 -0500

    Rewrite build system as generic Makefiles
    The Makefiles are now identical and generic
    across all modules.  Additional 'config.mk'
    files have been added where the build settings
    and dependencies can be specified for each module.

---
+++ init/config.mk
@@ -0,0 +1,12 @@
+# ============== MODULE CONFIG ============== #
+
+MOD = init
+PRG = init-test
+EXE = init.hex
+
+MOD_HDR += init.h
+
+PRG_HDR += init.h
+
+PRG_OBJ += init.p1
+PRG_OBJ += init-test.p1

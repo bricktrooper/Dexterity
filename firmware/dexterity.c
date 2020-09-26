c95b25b971d2444a2eba1ed39dab4aa72cbe1cae
Kyle Pinto <kylepinto@yahoo.ca>
2020-09-26 15:42:06 -0400

    firmware: Add simple message recognition
    The PIC will accept simple messages and print a result to the UART.
    The message must be terminated by a carriage return [ENTER] keypress.

--- firmware/dexterity.c
+++ firmware/dexterity.c
@@ -1,6 +1,8 @@
 #include <xc.h>
 #include <pic16f690.h>
 
+#include <string.h>
+
 #include "pins.h"
 #include "utils.h"
 #include "init.h"
@@ -11,72 +13,74 @@
 #include "accel.h"
 #include "flex.h"
 
-struct Readings
+#define MAX_MESSAGE_LENGTH 10
+
+struct Hand
 {
 	int accel [3];
 	int flex [5];
 };
 
-enum PacketType
+enum Type
 {
 	SAMPLE,
 	DEBUG,
 	CALIBRATE
 };
 
-struct PacketHeader
+struct Header
 {
-	enum PacketType type;
+	enum Type type;
 	int length;
 };
 
+char * MESSAGES [] = {"sample", "debug", "calibrate"};
+
 void init(void);
-int sample(struct Readings * readings);
-void calibrate(void);
-int send(struct PacketHeader * header, char * data);
-int receive(struct PacketHeader * header, char * data);
+int sample(struct Hand * hand);
+void calibrate(struct Calibration * calibration);
+int send(struct Header * header, void * data);
+int receive(struct Header * header, void * data);
 
 void main(void)
 {
 	init();
-	struct Readings readings;
+
+	struct Hand hand;
+	char message [10];
 
 	while (1)
 	{
-		sample(&readings);
-		uart_print("X: %d Y: %d Z: %d F1: %d F2: %d F3: %d F4: %d F5: %d" NEWLINE,
-					readings.accel[X],
-					readings.accel[Y],
-					readings.accel[Z],
-					readings.flex[F1],
-					readings.flex[F2],
-					readings.flex[F3],
-					readings.flex[F4],
-					readings.flex[F5]
-					);
+		uart_scan(message, sizeof(message));
+
+		if (strcmp(message, MESSAGES[SAMPLE]) == 0)
+		{
+			sample(&hand);
+			// uart_transmit((char *)&hand, sizeof(hand));
+			uart_print("X: %d Y: %d Z: %d F1: %d F2: %d F3: %d F4: %d F5: %d" NEWLINE,
+						hand.accel[X],
+						hand.accel[Y],
+						hand.accel[Z],
+						hand.flex[F1],
+						hand.flex[F2],
+						hand.flex[F3],
+						hand.flex[F4],
+						hand.flex[F5]
+						);
+		}
+		else if (strcmp(message, MESSAGES[DEBUG]) == 0)
+		{
+			uart_print("Dexterity is running" NEWLINE);
+		}
+		else if (strcmp(message, MESSAGES[CALIBRATE]) == 0)
+		{
+			// calibrate here
+		}
+		else
+		{
+			// unknown packet
+		}
 	}
-}
-
-int sample(struct Readings * readings)
-{
-	if (readings == null)
-	{
-		return ERROR;
-	}
-
-	adc_set_vref(EXTERNAL);
-	readings->accel[X] = accel_read(X);
-	readings->accel[Y] = accel_read(Y);
-	readings->accel[Z] = accel_read(Z);
-
-	adc_set_vref(INTERNAL);
-	readings->flex[F1] = flex_read(F1);
-	readings->flex[F2] = flex_read(F2);
-	readings->flex[F3] = flex_read(F3);
-	readings->flex[F4] = flex_read(F4);
-	readings->flex[F5] = flex_read(F5);
-
-	return SUCCESS;
 }
 
 void init(void)
@@ -88,4 +92,26 @@ void init(void)
 	adc_init();
 	accel_init();
 	flex_init();
+}
+
+int sample(struct Hand * hand)
+{
+	if (hand == null)
+	{
+		return ERROR;
+	}
+
+	adc_set_vref(EXTERNAL);
+	hand->accel[X] = accel_raw(X);
+	hand->accel[Y] = accel_raw(Y);
+	hand->accel[Z] = accel_raw(Z);
+
+	adc_set_vref(INTERNAL);
+	hand->flex[F1] = flex_raw(F1);
+	hand->flex[F2] = flex_raw(F2);
+	hand->flex[F3] = flex_raw(F3);
+	hand->flex[F4] = flex_raw(F4);
+	hand->flex[F5] = flex_raw(F5);
+
+	return SUCCESS;
 }

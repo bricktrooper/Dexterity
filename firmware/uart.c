524125c5e0cc3f2f355befd41c94631a7d231f6f
Kyle Pinto <kylepinto@yahoo.ca>
2020-09-30 00:07:37 -0400

    firmware: Check for RX buffer overrun
    If data comes in too fast it will overrun the RX buffer
    and lock the receiver.  Check for this while looping in
    uart_receive() and reset the receiver if necessary.

--- firmware/uart.c
+++ firmware/uart.c
@@ -80,6 +80,12 @@ int uart_receive(char * data, int size)
 
 	for (int i = 0; i < size; i++)
 	{
+		if (RCSTAbits.OERR == 1)   // Reset receiver if RX buffer is overrun
+		{
+			RCSTAbits.CREN = 0;
+			RCSTAbits.CREN = 1;
+		}
+
 		data[i] = uart_receive_byte();
 		count++;
 	}
